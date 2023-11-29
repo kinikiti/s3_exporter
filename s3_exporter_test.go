f63aae57aa8c554e6546cebe93efcc0a2f0423f4
Kirill Nikitin <kirill@16v.ru>
2023-11-29 13:57:11 +0100

    Test suite

--- s3_exporter_test.go
+++ s3_exporter_test.go
@@ -44,33 +44,6 @@ var (
 				Prefix:      String("one"),
 			},
 		},
-		s3ExporterTestCase{
-			Name:   "one object",
-			Bucket: "mock",
-			Prefix: "one",
-			ExpectedOutputLines: []string{
-				"s3_list_success{bucket=\"mock\",delimiter=\"\",prefix=\"one\"} 1",
-				"s3_last_modified_object_date{bucket=\"mock\",prefix=\"one\"} 1.5604596e+09",
-				"s3_last_modified_object_size_bytes{bucket=\"mock\",prefix=\"one\"} 1234",
-				"s3_biggest_object_size_bytes{bucket=\"mock\",prefix=\"one\"} 1234",
-				"s3_objects_size_sum_bytes{bucket=\"mock\",prefix=\"one\"} 1234",
-				"s3_objects{bucket=\"mock\",prefix=\"one\"} 1",
-			},
-			ListObjectVersionsResponse: &s3.ListObjectVersionsOutput{
-				Contents: []*s3.Object{
-					&s3.Object{
-						Key:          String("one"),
-						LastModified: Time(time.Date(2019, time.June, 13, 21, 0, 0, 0, time.UTC)),
-						Size:         Int64(1234),
-					},
-				},
-				IsTruncated: Bool(false),
-				KeyCount:    Int64(1),
-				MaxKeys:     Int64(1000),
-				Name:        String("mock"),
-				Prefix:      String("one"),
-			},
-		},
 		// Test no matching objects in the bucket
 		s3ExporterTestCase{
 			Name:   "no objects",
@@ -201,6 +174,17 @@ func (tcs *s3ExporterTestCases) response(bucket, prefix string) (*s3.ListObjects
 	return nil, errors.New("Can't find a response for the bucket and prefix combination")
 }
 
+// Returns the mocked response for a bucket+prefix combination
+func (tcs *s3ExporterVersionsTestCases) responsev(bucket, prefix string) (*s3.ListObjectVersionsOutput, error) {
+	for _, c := range *tcs {
+		if c.Bucket == bucket && c.Prefix == prefix {
+			return c.ListObjectVersionsOutput, nil
+		}
+	}
+
+	return nil, errors.New("Can't find a response for the bucket and prefix combination")
+}
+
 // TestProbeHandler iterates over a list of test cases
 func TestProbeHandler(t *testing.T) {
 	for _, c := range testCases {
@@ -225,7 +209,7 @@ func (m *mockS3Client) ListObjectsV2(input *s3.ListObjectsV2Input) (*s3.ListObje
 
 // ListObjectVersions mocks out the corresponding function in the S3 client, returning the response that corresponds to the test case
 func (m *mockS3Client) ListObjectVersions(input *s3.ListObjectVersionsInput) (*s3.ListObjectVersionsOutput, error) {
-	r, err := testCases.response(*input.Bucket, *input.Prefix)
+	r, err := testCases.responsev(*input.Bucket, *input.Prefix)
 	if err != nil {
 		return nil, err
 	}
